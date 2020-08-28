617b30ec52dd81ebbcf434b248e6670b1a3c1e96
Wasd5 <zhao115@usc.edu>
2020-08-28 16:30:53 -0700

    py client

--- ee250/lab03/tcp_client.py
+++ ee250/lab03/tcp_client.py
@@ -1,9 +1,9 @@
 """
-Server IP is 52.88.20.156, ports are 5000-5008, socket is UNIX TCP 
 Server receiver buffer is char[256]
 If correct, the server will send a message back to you saying "I got your message"
 Write your socket client code here in python
 Establish a socket connection -> send a short message -> get a message back -> ternimate
+use python "input->" function, enter a line of a few letters, such as "abcd"
 """
 import socket
 
